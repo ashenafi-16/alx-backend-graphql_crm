4dec6c8097f9f20e2c21e95c1f35fc147a7231d1
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2025-08-24 23:37:52 +0300

    add manage.py

---
+++ alx-backend-graphql_crm/settings.py
@@ -0,0 +1,21 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # GraphQL
+    "graphene_django",
+    "crm",
+]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"  # point to schema
+}
+
+DEBUG = True   # during development
+
+ALLOWED_HOSTS = ["*"]  # allow all hosts for local dev
+
