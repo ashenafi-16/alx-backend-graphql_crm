9c6aeba60fab71df082b4cfbce38ca8c30710582
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2025-08-24 23:43:07 +0300

    add manage.py

--- crm/filters.py
+++ crm/filters.py
@@ -2,17 +2,21 @@ import django_filters
 from .models import Customer, Product, Order
 
 class CustomerFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr="icontains")
-    email = django_filters.CharFilter(lookup_expr="icontains")
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+    email = django_filters.CharFilter(field_name="email", lookup_expr="icontains")
     created_at__gte = django_filters.DateFilter(field_name="created_at", lookup_expr="gte")
     created_at__lte = django_filters.DateFilter(field_name="created_at", lookup_expr="lte")
+    phone_pattern = django_filters.CharFilter(method="filter_phone_pattern")
+
+    def filter_phone_pattern(self, queryset, name, value):
+        return queryset.filter(phone__startswith=value)
 
     class Meta:
         model = Customer
         fields = ["name", "email", "created_at"]
 
 class ProductFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr="icontains")
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     price__gte = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     price__lte = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
     stock__gte = django_filters.NumberFilter(field_name="stock", lookup_expr="gte")
@@ -29,6 +33,7 @@ class OrderFilter(django_filters.FilterSet):
     order_date__lte = django_filters.DateFilter(field_name="order_date", lookup_expr="lte")
     customer_name = django_filters.CharFilter(field_name="customer__name", lookup_expr="icontains")
     product_name = django_filters.CharFilter(field_name="products__name", lookup_expr="icontains")
+    product_id = django_filters.NumberFilter(field_name="products__id")
 
     class Meta:
         model = Order
